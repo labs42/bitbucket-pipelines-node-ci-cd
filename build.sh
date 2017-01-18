d05eb9cafab5e7373976c038b93751ea328578a1
Alexander Antipin <alex@antipin.com>
2017-01-18 06:08:13 +0300

    Move Dockerfile to src directoory, add create_id_rsa script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build . -t labs42/bitbucket-pipelines-node-ci-cd:latest
+docker build ./src -t labs42/bitbucket-pipelines-node-ci-cd:latest
+docker push labs42/bitbucket-pipelines-node-ci-cd:latest
